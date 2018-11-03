6fa316fcf8df91f7acb35518f943aaabec0dbd60
Richard Ju <richarlj@usc.edu>
2018-11-02 19:56:32 -0700

    added stuff

---
+++ Visceroll/Assets/GameNarrativeManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameNarrativeManager : MonoBehaviour {
+
+	// Use this for initialization
+	[System.Serializable]
+	public class Task
+	{
+		public AudioClip StartTaskClip, CompletedTaskClip;
+		public List<AudioClip> DuringTaskClip;
+		public string StartTaskText, CompletedText;
+		public UnityEvent OnStartTask, OnCompleteTask;
+
+	}
+
+	public List<Task> Jobs;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
